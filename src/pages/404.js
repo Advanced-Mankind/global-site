28761410fadcf42cfb73464dbac54a853a4e3667
DulceFabiola <62074962+DulceFabiola@users.noreply.github.com>
2020-12-17 13:25:30 -0600

    MessageError

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,48 @@
-import { Link } from "gatsby";
 import React from "react";
+import styles from "../theme/index.module.css";
 import Layout from "../components/layout";
-
+import "../components/Error404/styles.css";
 export default function Error404() {
   return (
     <Layout>
-      <Link to="/404/">Error404</Link>
+      <div className={`row align-items-center m-0`}>
+        <div className="col-sm-5 p-0">
+          <div className={`${styles.leftContent}`}>
+            <div className="row">
+              <div className="col-12 mt-3">
+                <img
+                  className={styles.logoOops}
+                  src={require("../../static/oops.png")}
+                  alt=""
+                />
+                <h1 className={styles.title}>That's no good! </h1>
+              </div>
+              <div className="col-12">
+                <p className={`${styles.subTitle} mb-0`}>
+                  We are going to send you back to a safe space where things
+                  don't wilt or die.
+                </p>
+              </div>
+            </div>
+          </div>
+          <img
+            className={styles.background_mobile}
+            src={require("../../static/messageError.png")}
+            alt=""
+          />
+        </div>
+
+        <div className="d-none d-sm-block col-sm-7 p-0 align-self-end">
+          <img
+            src={require("../../static/messageError.png")}
+            className={styles.background_tablet}
+            alt=""
+          />
+        </div>
+        <div className={styles.curveBackgroundBottom}>
+          <div className="curveBackgroundHeight"></div>
+        </div>
+      </div>
     </Layout>
   );
 }
