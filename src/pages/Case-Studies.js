d396d00bd2d24888d062ca2c9581767c17208c33
Dulce Fabiola <dulcefabiola.rm@gmail.com>
2021-02-05 02:31:40 -0600

    CaseStudies2021

--- src/pages/Case-Studies.js
+++ src/pages/Case-Studies.js
@@ -13,12 +13,6 @@ export default function CaseStudies() {
       />
       <div style={{ marginTop: '5%' }} >
         <CaseStudyPageImagesSection
-          desktopImgs={[require('../../static/caseStudiesPageMotoDesktop.png'), require('../../static/caseStudiesPageChedrahuiDesktop.png')]}
-          tabletImgs={[require('../../static/caseStudiesPageMotoTablet.png'), require('../../static/caseStudiesPageChedrahuiTablet.png')]}
-          mobileImgs={[require('../../static/caseStudiesPageMotoMobile.png'), require('../../static/caseStudiesPageChedrahuiMobile.png')]}
-          chedrahuiDesktop={[require('../../static/caseStudiesPagePhone1Desktop.png'), require('../../static/caseStudiesPagePhone2Desktop.png'), require('../../static/caseSudiesPageLaptopDesktop.png')]}
-          chedrahuiTablet={[require('../../static/caseStudiesPagePhone1Tablet.png'), require('../../static/caseStudiesPagePhone2Tablet.png'), require('../../static/caseSudiesPageLaptopTablet.png')]}
-          chedrahuiMobile={[require('../../static/caseStudiesPagePhone1Mobile.png'), require('../../static/caseStudiesPagePhone2Mobile.png'), require('../../static/caseSudiesPageLaptopMobile.png')]}
         />
       </div>
       <div style={{ marginTop: "5%" }}>
