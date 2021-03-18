732a44eda7f5d6d4f25b284b49c66bd0fcd8ef7d
Dulce Fabiola <dulcefabiola.rm@gmail.com>
2021-03-17 19:44:23 -0600

    Fixs

--- src/components/OurPerks/OurPerkQuote.js
+++ src/components/OurPerks/OurPerkQuote.js
@@ -3,15 +3,6 @@ import { Col, Container, Row } from "react-bootstrap";
 const OurPerkQuote = () => {
   return (
     <div>
-      <Container className="ourPerkQuoteContainer">
-        <h1 className="quoteTitle">
-          “Can’t ignore the great perks! We work hard every day so we can have a
-          healthy work/life balance”
-        </h1>
-        <p className="quoteInfo">SCOTT PRATHER</p>
-        <p className="quoteFoot">Creative Director</p>
-      </Container>
-
       <Container  className="ourPerkQuoteContainer">
         <h1 className="quoteTitle">
           "I found a place with interesting challenges that allows me to be
