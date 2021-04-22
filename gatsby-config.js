9f8105005414cdc4ed1a11095359991b2fc420f9
alerodriguez162 <alerodriguez162@gmail.com>
2021-04-22 11:07:32 -0500

    Favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,5 +38,17 @@ module.exports = {
       resolve: "gatsby-source-contentful",
       options: contentfulConfig,
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "React go",
+        short_name: "Reactgo",
+        start_url: "/",
+        background_color: "#f7f0eb",
+        theme_color: "#a2466c",
+        display: "standalone",
+        icon: "src/images/Favicon.png",
+      },
+    },
   ],
 };
